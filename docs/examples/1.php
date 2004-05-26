5a96324808a2c1447e7a894d7edaf8d0164c582b
Harry Fuecks <hfuecks@php.net>
2004-05-26 20:35:00 +0000

    Small to to see if Ive got Karma
    git-svn-id: http://svn.php.net/repository/pear/packages/Calendar/trunk@159744 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/1.php
+++ docs/examples/1.php
@@ -3,6 +3,7 @@
 * Description: Passes through all main calendar classes, beginning with year
 * and down to seconds, skipping weeks. Useful to test Calendar is (basically)
 * working correctly
+*
 */
 function getmicrotime(){
     list($usec, $sec) = explode(" ",microtime());

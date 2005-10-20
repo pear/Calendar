d1c97ddaa9420c0d837c953507960c77abb3b7ae
Lorenzo Alberton <quipo@php.net>
2005-10-20 18:59:45 +0000

    choose one: week_test (firstDay=monday) or week_firstday_0_test (firstDay=sunday)
    git-svn-id: http://svn.php.net/repository/pear/packages/Calendar/trunk@198886 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/calendar_tabular_tests.php
+++ tests/calendar_tabular_tests.php
@@ -10,6 +10,7 @@ class CalendarTabularTests extends GroupTest {
         $this->addTestFile('month_weekdays_test.php');
         $this->addTestFile('month_weeks_test.php');
         $this->addTestFile('week_test.php');
+        //$this->addTestFile('week_firstday_0_test.php'); //switch with the above
     }
 }
 

c98b7c765710c0557b61438beb578002ca8d5833
Henrik Hansen <hh@fsck.dk>
2019-04-15 10:39:28 +0200

    Warning fix
    Each is deprecated in 7.2

--- Calendar/Calendar.php
+++ Calendar/Calendar.php
@@ -403,7 +403,10 @@ class Calendar
      */
     function fetch()
     {
-        $child = each($this->children);
+        $key = key($this->children);
+        $child = ($key === null) ? false : [$key, current($this->children), 'key' => $key, 'value' => current($this->children)];
+        next($this->children);
+
         if ($child) {
             return $child['value'];
         } else {
